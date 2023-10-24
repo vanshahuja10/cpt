6582be5af23874331efb1948169fafdad3fe0a0d
vanshahuja10 <vanshahuja266@gmail.com>
2023-10-24 15:59:12 +0530

    intial

---
+++ checking/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+# setting up the server for installing nginx and nodejs
+sudo su
+
+apt update
+
+apt install nginx -y 
+#installing nginx at the server
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash 
+
+apt install nodejs -y
+#installing node js at the server
+git clone -b master https://github.com/Manav-Khandurie/AWSNETFLIXSAAS.git
+
+npm install pm2@latest -g
+#installing pm2 at the server
+cd AWSNETFLIXSAAS/netflix-api
+
+npm install
+#installs dependencies
+cd ..
+
+cd checking
+
+rm /etc/nginx/sites-available/default
+
+mv default /etc/nginx/sites-available/default
+#adds the custom ngnix cong file
+cd ..
+
+cd netflix-api
+
+pm2 start server.js --name netflix 
+#starts SAAS application
+sudo service nginx restart
+#restarts the service
